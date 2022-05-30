0edf6b5c497f22271a215475869cd7a20444adce
Chibuike Tim Okonkwo <chibuiketimokonkwo@gmail.com>
2022-05-30 13:54:29 +0100

    https

--- js/forcast.js
+++ js/forcast.js
@@ -4,7 +4,7 @@ const key = 'KzU5dSaTfvrfdZgsg3pFVH77Z03clhbq';
 // Get weather information
 const getWeather = async(id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
 
     const query = `${id}?apikey=${key}`;
 
@@ -21,7 +21,7 @@ const getWeather = async(id) => {
 // Get city information
 const getCity = async(city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
 
     const query = `?apikey=${key}&q=${city}`;
 
